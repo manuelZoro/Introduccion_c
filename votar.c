1d1f712f0218701a806f2c9a4b5b9100ea29569d
Manuel <zoro@ManuelMtz.(none)>
2014-07-09 21:47:58 -0500

    Codigos de introduccion a C

---
+++ votar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (){
+int n;
+printf("Dame tu edad");
+scanf("%d",&n);
+
+if(n>=18){
+ printf("Si puedes votar \n");
+
+}
+
+else{
+ printf("No puedes votar \n");
+}
+return 0;
+}
